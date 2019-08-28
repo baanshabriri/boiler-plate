2df1696bde2471bc92dbca584ea97df118dbba60
ShabririB <abhinandandas77@gmail.com>
2019-08-28 16:46:08 +0530

    first commit

---
+++ API/src/__init__.py
@@ -0,0 +1,11 @@
+from .config import configs
+from .utils import api, db, ma, create_app, ReprMixin, bp, BaseMixin, admin, BaseSchema, BaseView, \
+    AssociationView, celery, serializer_helper,\
+    sentry, sms, url_shortener, limiter, DataResource, DataView, jwt, razor
+
+from .admin_panel import admin_manager
+
+from .user import models, schemas, views
+from .reports import views
+
+from .utils.security import security
---
+++ API/src/admin_panel/admin_manager.py
@@ -0,0 +1,22 @@
+from flask_admin.contrib.sqla import ModelView
+
+from flask_security import current_user
+
+from src import admin, db
+
+from src.user.models import User, Role, UserRole, UserToUser
+
+class MyModel(ModelView):
+    page_size = 100
+    can_set_page_size = True
+    can_view_details = True
+
+    def is_accessible(self):
+        return current_user.has_role('admin')
+
+
+admin.add_view(MyModel(User, session=db.session))
+admin.add_view(MyModel(Role, session=db.session))
+admin.add_view(MyModel(UserRole, session=db.session))
+
+admin.add_view(MyModel(UserToUser, session=db.session))
